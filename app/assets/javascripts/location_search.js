8c9d586890b60b9a88ed1edd450a0badad6eb0ec
Vesa Luusua <vesa.luusua@gmail.com>
2016-06-20 18:59:17 +0300

    Clear derivative hidden fields values.

--- app/assets/javascripts/location_search.js
+++ app/assets/javascripts/location_search.js
@@ -12,7 +12,14 @@ window.ST = window.ST || {};
     var autocomplete = new window.google.maps.places.Autocomplete(searchInput, { bounds: { north: -90, east: -180, south: 90, west: 180 } });
     autocomplete.setTypes(['geocode']);
 
-    boundingboxInput.value = null;
+    // The values of these fields are defined based on the value of searchInput
+    function clearHiddenInputs() {
+      statusInput.value = null;
+      coordinateInput.value = null;
+      boundingboxInput.value = null;
+      maxDistanceInput.value = null;
+    }
+    clearHiddenInputs();
 
     function toRadians(degrees) {
       return degrees * (Math.PI/180);
