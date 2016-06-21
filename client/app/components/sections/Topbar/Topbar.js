667106fd1d533e186fdb8a391b5012b47e36a007
Kimmo Puputti <kimmo@sharetribe.com>
2016-06-21 10:28:48 +0300

    Change ported code to please ESLint

--- client/app/components/sections/Topbar/Topbar.js
+++ client/app/components/sections/Topbar/Topbar.js
@@ -92,33 +92,38 @@ const DEFAULT_CONTEXT = {
   loggedInUsername: null,
 };
 
-function toRadians(degrees) {
-  return degrees * (Math.PI/180);
-}
+const toRadians = (degrees) => degrees * (Math.PI / 180); // eslint-disable-line no-magic-numbers
 
-function computeScale(a, b) {
-  var R = 6371; // Earth's radius in km
+const computeScale = (pointA, pointB) => {
+  /* eslint-disable no-magic-numbers */
 
-  var lat1 = a.lat();
-  var lat2 = b.lat();
-  var lng1 = a.lng();
-  var lng2 = b.lng();
-  var lat1InRadians = toRadians(lat1);
-  var lat2InRadians = toRadians(lat2);
-  var latDiffInRadians = toRadians(lat2-lat1);
-  var lngDiffInRadians = toRadians(lng2-lng1);
+  const EARTH_RADIUS = 6371;
+
+  const lat1 = pointA.lat();
+  const lat2 = pointB.lat();
+  const lng1 = pointA.lng();
+  const lng2 = pointB.lng();
+  const lat1InRadians = toRadians(lat1);
+  const lat2InRadians = toRadians(lat2);
+  const latDiffInRadians = toRadians(lat2 - lat1);
+  const lngDiffInRadians = toRadians(lng2 - lng1);
 
   // The haversine formula
   // 'a' is the square of half the chord length between the points
-  var a = Math.sin(latDiffInRadians/2) * Math.sin(latDiffInRadians/2) +
-        Math.cos(lat1InRadians) * Math.cos(lat2InRadians) *
-        Math.sin(lngDiffInRadians/2) * Math.sin(lngDiffInRadians/2);
+  const a = Math.sin(latDiffInRadians / 2) * Math.sin(latDiffInRadians / 2) +
+          Math.cos(lat1InRadians) * Math.cos(lat2InRadians) *
+          Math.sin(lngDiffInRadians / 2) * Math.sin(lngDiffInRadians / 2);
+
   // the angular distance in radians
-  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
   // distance between coordinates
-  var d = R * c;
-  return d/2;
-}
+  const d = EARTH_RADIUS * c;
+
+  return d / 2;
+
+  /* eslint-enable no-magic-numbers */
+};
 
 const coordinates = (place) => {
   if (place && place.geometry) {
